f98c7922b51307d5aa64ee81eae965bf79e90f34
Grant <grantferment@gmail.com>
2020-12-07 21:02:52 -0500

    inital commit

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS emp_tracker_db;
+CREATE DATABASE emp_tracker_db;
+USE emp_tracker_db;
+
+CREATE TABLE department {
+    id INT AUTO_INCREMENT,
+    name VARCHAR(30), 
+    PRIMARY KEY (id)
+};
+
+CREATE TABLE role {
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NULL,
+    salary DECIMAL(10,2) NULL,
+    department_id INT,
+    PRIMARY KEY (id)
+};
+
+CREATE TABLE employee {
+    id INT AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT NULL,
+};
+
